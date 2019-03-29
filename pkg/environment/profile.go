ff8cdc2691d7e1e0bef91f101f29bc3182345c7c
Brian Buchanan <bpbuch@gmail.com>
2019-03-29 13:26:55 -0400

    [FAB-14515] Encapsulate Fabric GO SDK
    Encapsulated the various clients provided by the Fabric Go SDK.  The
    encapsulation will provide a starting point for potential future
    implementation of longer living clients.
    
    Implemented profile to SDK config transformation using templates.  The
    template serves as a dynamic way to generate sdk config yaml.  The
    transformation takes place when any SDK client is instantiated.
    
    Generated mocks for SDK clients.
    
    Change-Id: I10586dca42c111be38a5279b439446f0f998bf05
    Signed-off-by: Brian Buchanan <bpbuch@gmail.com>

--- pkg/environment/profile.go
+++ pkg/environment/profile.go
@@ -6,7 +6,72 @@ SPDX-License-Identifier: Apache-2.0
 
 package environment
 
+import (
+	"bytes"
+	"text/template"
+)
+
 // Profile contains metadata for a fabric network
 type Profile struct {
 	Name string
+
+	Context         *Context
+	CryptoConfig    string
+	CredentialStore string
+
+	Organizations map[string]*Organization
+	Orderers      map[string]*Orderer
+	Peers         map[string]*Peer
+}
+
+// Context contains the active focus of the profile
+type Context struct {
+	Organization string
+	Identity     string
+	Channel      string
+	Orderers     []string
+	Peers        []string
+}
+
+// Organization contains configuration details for an organization
+type Organization struct {
+	ID    string
+	MSP   *MSP
+	Peers []string
+}
+
+// MSP contains configuration details for a msp
+type MSP struct {
+	ID    string
+	Store string
+}
+
+// Orderer contains configuration details for a orderer
+type Orderer struct {
+	ID  string
+	URL string
+	TLS string
+}
+
+// Peer contains configuration details for a peer
+type Peer struct {
+	ID       string
+	URL      string
+	EventURL string
+	TLS      string
+}
+
+// ToTemplate transforms the profile into the provided template
+func (p *Profile) ToTemplate(path string) ([]byte, error) {
+	tmpl, err := template.ParseFiles(path)
+	if err != nil {
+		return nil, err
+	}
+
+	buffer := &bytes.Buffer{}
+	if err := tmpl.Execute(buffer, p); err != nil {
+		return nil, err
+	}
+
+	return buffer.Bytes(), nil
 }
